edc75519d6e210c8ed3c0d798610efb0375c6e81
ShashankShekhar9839 <dj78390@gmail.com>
2023-01-23 00:54:10 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,554 @@
+const variantData = [
+  {
+    verlabel: "VXI",
+    isDiscontinued: false,
+    verid: "5547",
+    verprice: "₹ 12.09 Lakhs",
+    verinfo: {
+      fuelType: { itemId: 1, customId: 11, value: "diesel" },
+      TransimissionType: { itemId: 2, customId: 21, value: "Automatic CVT" },
+      SeaterType: { itemId: 3, customId: 32, value: "5 Seater" },
+    },
+    onClick: null,
+  },
+  {
+    verlabel: "LDI",
+    isDiscontinued: false,
+    verid: "5547",
+    verprice: "₹ 12.09 Lakhs",
+    verinfo: {
+      fuelType: { itemId: 1, customId: 13, value: "Petorl" },
+      TransimissionType: { itemId: 2, customId: 22, value: "Manual" },
+      SeaterType: { itemId: 3, customId: 33, value: "7 Seater" },
+    },
+    onClick: null,
+  },
+  {
+    verlabel: "VXI",
+    isDiscontinued: false,
+    verid: "5547",
+    verprice: "₹ 12.09 Lakhs",
+    verinfo: {
+      fuelType: { itemId: 1, customId: 12, value: "CNG" },
+      TransimissionType: { itemId: 2, customId: 23, value: "Automatic + Manual" },
+      SeaterType: { itemId: 3, customId: 31, value: "3 Seater" },
+    },
+    onClick: null,
+  },
+  {
+    verlabel: "VXI",
+    isDiscontinued: false,
+    verid: "5547",
+    verprice: "₹ 12.09 Lakhs",
+    verinfo: {
+      fuelType: { itemId: 1, customId: 11, value: "diesel" },
+      TransimissionType: { itemId: 2, customId: 23, value: "Automatic + Manual" },
+      SeaterType: { itemId: 3, customId: 31, value: "3 Seater" },
+    },
+    onClick: null,
+  },
+  {
+    verlabel: "VXI",
+    verid: "5547",
+    verprice: "₹ 12.09 Lakhs",
+    verinfo: {
+      fuelType: { itemId: 1, customId: 11, value: "diesel" },
+      TransimissionType: { itemId: 2, customId: 22, value: "Manual" },
+      SeaterType: { itemId: 3, customId: 31, value: "3 Seater" },
+    },
+    onClick: null,
+  },
+  {
+    verlabel: "LDI",
+    isDiscontinued: false,
+    verid: "5547",
+    verprice: "₹ 12.09 Lakhs",
+    verinfo: {
+      fuelType: { itemId: 1, customId: 11, value: "diesel" },
+      TransimissionType: { itemId: 2, customId: 22, value: "Manual" },
+      SeaterType: { itemId: 3, customId: 33, value: "7 Seater" },
+    },
+    onClick: null,
+  },
+  {
+    verlabel: "VXI",
+    isDiscontinued: false,
+    verid: "5547",
+    verprice: "₹ 12.09 Lakhs",
+    verinfo: {
+      fuelType: { itemId: 1, customId: 11, value: "diesel" },
+      TransimissionType: { itemId: 2, customId: 22, value: "Manual" },
+      SeaterType: { itemId: 3, customId: 34, value: "8 Seater" },
+    },
+    onClick: null,
+  },
+  {
+    verlabel: "VXI",
+    isDiscontinued: false,
+    verid: "5547",
+    verprice: "₹ 12.09 Lakhs",
+    verinfo: {
+      fuelType: { itemId: 1, customId: 12, value: "CNG" },
+      TransimissionType: { itemId: 2, customId: 23, value: "Automatic + Manual" },
+      SeaterType: { itemId: 3, customId: 31, value: "3 Seater" },
+    },
+    onClick: null,
+  },
+
+
+  {
+    verlabel: "LDI",
+    isDiscontinued: true,
+    verid: "5547",
+    verprice: "₹ 12.09 Lakhs",
+    verinfo: {
+      fuelType: { itemId: 1, customId: 11, value: "diesel" },
+      TransimissionType: { itemId: 2, customId: 21, value: "Automatic CVT" },
+      SeaterType: { itemId: 3, customId: 32, value: "5 Seater" },
+    },
+    onClick: null,
+  },
+
+  {
+    verlabel: "VXI",
+    isDiscontinued: true,
+    verid: "5547",
+    verprice: "₹ 12.09 Lakhs",
+    verinfo: {
+      fuelType: { itemId: 1, customId: 12, value: "CNG" },
+      TransimissionType: { itemId: 2, customId: 23, value: "Automatic + Manual" },
+      SeaterType: { itemId: 3, customId: 31, value: "3 Seater" },
+    },
+    onClick: null,
+  },
+  {
+    verlabel: "LDI",
+    isDiscontinued: true,
+    verid: "5547",
+    verprice: "₹ 12.09 Lakhs",
+    verinfo: {
+      fuelType: { itemId: 1, customId: 11, value: "diesel" },
+      TransimissionType: { itemId: 2, customId: 22, value: "Manual" },
+      SeaterType: { itemId: 3, customId: 34, value: "8 Seater" },
+    },
+    onClick: null,
+  },
+  {
+    verlabel: "VXI",
+    isDiscontinued: true,
+    verid: "5547",
+    verprice: "₹ 12.09 Lakhs",
+    verinfo: {
+      fuelType: { itemId: 1, customId: 12, value: "CNG" },
+      TransimissionType: { itemId: 2, customId: 22, value: "Manual" },
+      SeaterType: { itemId: 3, customId: 34, value: "8 Seater" },
+    },
+    onClick: null,
+  },
+  {
+    verlabel: "LDI",
+    isDiscontinued: true,
+    verid: "5547",
+    verprice: "₹ 12.09 Lakhs",
+    verinfo: {
+      fuelType: { itemId: 1, customId: 11, value: "diesel" },
+      TransimissionType: { itemId: 2, customId: 21, value: "Automatic CVT" },
+      SeaterType: { itemId: 3, customId: 32, value: "5 Seater" },
+    },
+    onClick: null,
+  },
+  {
+    verlabel: "VXI",
+    isDiscontinued: true,
+    verid: "5547",
+    verprice: "₹ 12.09 Lakhs",
+    verinfo: {
+      fuelType: { itemId: 1, customId: 12, value: "CNG" },
+      TransimissionType: { itemId: 2, customId: 22, value: "Automatic" },
+      SeaterType: { itemId: 3, customId: 31, value: "3 Seater" },
+    },
+    onClick: null,
+  },
+
+
+
+
+];
+
+
+
+let petrol = document.getElementById('petrol');
+let cng = document.getElementById('cng');
+let automatic = document.getElementById('automatic');
+let manual = document.getElementById('manual');
+
+let varientContainer = document.querySelector('.varient-container')
+
+console.log(variantData.length);
+
+function mapElements() {
+  for (let i = 0; i < variantData.length; i++) {
+    let varientCard = document.createElement('div');
+    varientCard.className = `cardElement active ${variantData[i].verinfo.fuelType.customId} ${variantData[i].verinfo.TransimissionType.customId}`
+    varientCard.innerHTML = ` <div class="card">
+    <h3>${variantData[i].verlabel}</h3>
+    <diV>
+        <span>${variantData[i].verinfo.fuelType.value.toUpperCase()}</span>
+        <span>${variantData[i].verinfo.TransimissionType.value}</span>
+    </diV>
+</div>`
+    varientContainer.append(varientCard);
+  }
+}
+
+if (petrol.checked == false && cng.checked == false && automatic.checked == false && manual.checked == false) {
+  mapElements();
+}
+
+
+
+
+let cards = document.querySelectorAll('.cardElement');
+console.log(cards.length);
+
+
+// handling petrol checkbox
+
+petrol.addEventListener('change', function () {
+  let pv = petrol.value;
+  console.log(typeof (pv));
+
+  if (petrol.checked == true) {
+    for (let i = 0; i < cards.length; i++) {
+      if (cards[i].classList.contains(petrol.value)) {
+        cards[i].style.display = 'block';
+      }
+      else {
+        cards[i].style.display = 'none';
+      }
+    }
+  }
+
+  if (petrol.checked == true && cng.checked == true) {
+    for (let i = 0; i < cards.length; i++) {
+      if (!cards[i].classList.contains(petrol.value) && !cards[i].classList.contains(cng.value)) {
+        cards[i].style.display = 'none';
+      }
+      else {
+        cards[i].style.display = 'block';
+      }
+    }
+  }
+
+  if (petrol.checked == false && cng.checked == true) {
+    for (let i = 0; i < cards.length; i++) {
+      if (!cards[i].classList.contains(cng.value)) {
+        cards[i].style.display = 'none';
+      }
+      else {
+        cards[i].style.display = 'block';
+      }
+    }
+  }
+
+
+  if (petrol.checked == true && manual.checked == true) {
+    for (let i = 0; i < cards.length; i++) {
+      if (!cards[i].classList.contains(petrol.value) && !cards[i].classList.contains(manual.value)) {
+        cards[i].style.display = 'none';
+      }
+      else {
+        cards[i].style.display = 'block';
+      }
+    }
+  }
+
+  if (manual.checked == true  && petrol.checked == true ) {
+    for (let i = 0; i < cards.length; i++) {
+      if (cards[i].classList.contains(manual.value) && cards[i].classList.contains(petrol.value)) {
+        cards[i].style.display = 'block';
+      }
+      else {
+        cards[i].style.display = 'none';
+      }
+    }
+  }
+
+
+
+  if (petrol.checked == true && automatic.checked == true) {
+    for (let i = 0; i < cards.length; i++) {
+      if (!cards[i].classList.contains(petrol.value) && !cards[i].classList.contains(automatic.value)) {
+        cards[i].style.display = 'none';
+      }
+      else {
+        cards[i].style.display = 'block';
+      }
+    }
+  }
+
+
+  if (automatic.checked == true && petrol.checked == true) {
+    for (let i = 0; i < cards.length; i++) {
+      if (cards[i].classList.contains(automatic.value) && cards[i].classList.contains(petrol.value)) {
+        cards[i].style.display = 'block';
+      }
+      else {
+        cards[i].style.display = 'none';
+      }
+    }
+  }
+
+  if(petrol.checked==true && automatic.checked==true && manual.checked==true){
+    for(let i=0;i<cards.length;i++){
+      if(cards[i].classList.contains(petrol.value)){
+        cards[i].style.display='block';
+      }
+      else{
+        cards[i].style.display='none';
+      }
+    }
+  }
+
+  if (petrol.checked == false && cng.checked == false && automatic.checked == false && manual.checked == false) {
+    for (let i = 0; i < cards.length; i++) {
+      cards[i].style.display = 'block';
+    }
+  }
+  if (petrol.checked == true && cng.checked == true && automatic.checked == true && manual.checked == true) {
+    mapElements();
+  }
+
+})
+
+
+// cng checkbox handling 
+cng.addEventListener('change', function () {
+  if (cng.checked == true) {
+    for (let i = 0; i < cards.length; i++) {
+      if (cards[i].classList.contains(cng.value)) {
+        cards[i].style.display = 'block';
+      }
+      else {
+        cards[i].style.display = 'none';
+      }
+    }
+  }
+
+  if (petrol.checked == true && cng.checked == true) {
+    for (let i = 0; i < cards.length; i++) {
+      if (!cards[i].classList.contains(petrol.value) && !cards[i].classList.contains(cng.value)) {
+        cards[i].style.display = 'none';
+      }
+      else {
+        cards[i].style.display = 'block';
+      }
+    }
+  }
+
+  if (petrol.checked == true && cng.checked == false) {
+    for (let i = 0; i < cards.length; i++) {
+      if (!cards[i].classList.contains(petrol.value)) {
+        cards[i].style.display = 'none';
+      }
+    }
+  }
+
+  if (cng.checked == true && automatic.checked == true) {
+    for (let i = 0; i < cards.length; i++) {
+      if (!cards[i].classList.contains(cng.value) && !cards[i].classList.contains(automatic.value)) {
+        cards[i].style.display = 'none';
+      }
+      else {
+        cards[i].style.display = 'block';
+      }
+    }
+  }
+
+
+  if (automatic.checked == true && cng.checked == true) {
+    for (let i = 0; i < cards.length; i++) {
+      if (cards[i].classList.contains(automatic.value) && cards[i].classList.contains(cng.value)) {
+        cards[i].style.display = 'block';
+      }
+      else {
+        cards[i].style.display = 'none';
+      }
+    }
+  }
+
+
+
+  if (cng.checked == false && petrol.checked == false && automatic.checked == false && manual.checked == false) {
+    for (let i = 0; i < cards.length; i++) {
+      cards[i].style.display = 'block';
+    }
+  }
+
+  if (petrol.checked == true && cng.checked == true && automatic.checked == true && manual.checked == true) {
+    mapElements();
+  }
+
+})
+
+// handling automatic checkboxes
+automatic.addEventListener('change', function () {
+  if (automatic.checked == true ) {
+    for (let i = 0; i < cards.length; i++) {
+      if (cards[i].classList.contains(automatic.value)) {
+        cards[i].style.display = 'block';
+      }
+      else {
+        cards[i].style.display = 'none';
+      }
+    }
+  }
+
+  if (petrol.checked == true && automatic.checked == true) {
+    for (let i = 0; i < cards.length; i++) {
+      if (cards[i].classList.contains(petrol.value) && cards[i].classList.contains(automatic.value)) {
+        cards[i].style.display = 'block';
+      }
+      else {
+        cards[i].style.display = 'none';
+      }
+    }
+  }
+
+  if (automatic.checked == false && petrol.checked == true) {
+    for (let i = 0; i < cards.length; i++) {
+      if (cards[i].classList.contains(petrol.value)) {
+        cards[i].style.display = 'block';
+      }
+      else {
+        cards[i].style.display = 'none';
+      }
+    }
+  }
+
+  if(automatic.checked==false && cng.checked==true){
+    for (let i = 0; i < cards.length; i++) {
+      if (cards[i].classList.contains(cng.value)) {
+        cards[i].style.display = 'block';
+      }
+      else {
+        cards[i].style.display = 'none';
+      }
+    }
+  }
+
+  if(petrol.checked==true && cng.checked==true && automatic.checked==true){
+    for(let i=0;i<cards.length;i++){
+      if(cards[i].classList.contains(petrol.value) && cards[i].classList.contains(cng.value)){
+        cards[i].style.display='block';
+      }
+      else{
+        cards[i].style.display='none';
+      }
+    }
+  }
+
+  
+
+  if (petrol.checked == false && cng.checked == false && automatic.checked == false && manual.checked == false) {
+    for (let i = 0; i < cards.length; i++) {
+      cards[i].style.display = 'block';
+
+    }
+  }
+})
+
+// hndling manual checkboxes
+
+manual.addEventListener('change',function(){
+        if(manual.checked==true){
+          for (let i = 0; i < cards.length; i++) {
+            if (cards[i].classList.contains(manual.value)) {
+              cards[i].style.display = 'block';
+            }
+            else {
+              cards[i].style.display = 'none';
+            }
+          }
+        }
+
+        if (petrol.checked == true && manual.checked == true) {
+          for (let i = 0; i < cards.length; i++) {
+            if (cards[i].classList.contains(petrol.value) && cards[i].classList.contains(manual.value)) {
+              cards[i].style.display = 'block';
+            }
+            else {
+              cards[i].style.display = 'none';
+            }
+          }
+        }
+      
+        if (manual.checked == false && petrol.checked == true) {
+          for (let i = 0; i < cards.length; i++) {
+            if (cards[i].classList.contains(petrol.value)) {
+              cards[i].style.display = 'block';
+            }
+            else {
+              cards[i].style.display = 'none';
+            }
+          }
+        }
+
+        if (manual.checked == false && petrol.checked == true && automatic.checked==true) {
+          for (let i = 0; i < cards.length; i++) {
+            if (cards[i].classList.contains(petrol.value) && cards[i].classList.contains(automatic.value)) {
+              cards[i].style.display = 'block';
+            }
+            else {
+              cards[i].style.display = 'none';
+            }
+          }
+        }
+
+        if (cng.checked == true && manual.checked == true) {
+          for (let i = 0; i < cards.length; i++) {
+            if (!cards[i].classList.contains(cng.value) && !cards[i].classList.contains(manual.value)) {
+              cards[i].style.display = 'none';
+            }
+            else {
+              cards[i].style.display = 'block';
+            }
+          }
+        }
+      
+        if (manual.checked == true  && cng.checked == true ) {
+          for (let i = 0; i < cards.length; i++) {
+            if (cards[i].classList.contains(manual.value) && cards[i].classList.contains(cng.value)) {
+              cards[i].style.display = 'block';
+            }
+            else {
+              cards[i].style.display = 'none';
+            }
+          }
+        }
+
+        if(manual.checked==false && cng.checked==true){
+          for (let i = 0; i < cards.length; i++) {
+            if (cards[i].classList.contains(cng.value)) {
+              cards[i].style.display = 'block';
+            }
+            else {
+              cards[i].style.display = 'none';
+            }
+          }
+        }
+
+        
+
+
+        if (petrol.checked == false && cng.checked == false && automatic.checked == false && manual.checked == false) {
+          for (let i = 0; i < cards.length; i++) {
+            cards[i].style.display = 'block';
+      
+          }
+        }    
+})
+
+
+
+
